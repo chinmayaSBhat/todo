dbd0dccc4d30227c133a399dc5d5452d2a843fc7
Chinmaya <chinmayasbhat4@gmail.com>
2020-06-28 17:10:32 +0530

    first commit

--- src/main/java/com/example/todo/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/todo/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.todo.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,8 +10,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-   
+    @Override
+	protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll() 
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll().and().exceptionHandling().accessDeniedPage("/error");
+
+        http.csrf().disable(); 
+        http.headers().frameOptions().disable(); 
+        http.formLogin().defaultSuccessUrl("/todo");
+    }
+
 
     
     @Override
